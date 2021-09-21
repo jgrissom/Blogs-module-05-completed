a0c6774363406324ea3fa9778cd99768bf6366d7
jgrissom <jgrissom91@gmail.com>
2021-09-21 12:21:14 -0500

    complete module 5

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using Blogs.Models;
+using System.Linq;
+
+namespace Blogs.Controllers
+{
+    public class HomeController : Controller
+    {
+        // this controller depends on the BloggingRepository
+        private BloggingContext _bloggingContext;
+        public HomeController(BloggingContext db) => _bloggingContext = db;
+
+        public IActionResult Index() => View(_bloggingContext.Blogs.OrderBy(b => b.Name));
+    }
+}
